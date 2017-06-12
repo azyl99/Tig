6759457b00724e8f7028ceaf0e417d3203faabca
zhangyilu <916548981@qq.com>
2017-06-13 00:48:31 +0800

    py

--- tools/convert_tree_to_json.py
+++ tools/convert_tree_to_json.py
@@ -4,14 +4,9 @@ import sys
 
 input = open("../tiger/"+sys.argv[1]+".txt", "r")
 str = input.read()
-
-pattern = re.compile(r'\[([^\[\]]*)')
-str = re.sub(pattern, lambda match: "{'name':'"+match.group(1)+"','children':[", str)
-pattern = re.compile(r']')
-str = re.sub(pattern, lambda match: "]},", str)
-pattern = re.compile(r",'children':\[\]")
-str = re.sub(pattern, lambda match: "", str)
-
+str = re.sub(r'\[([^\[\]]*)', lambda match: "{'name':'"+match.group(1)+"','children':[", str)
+str = re.sub(r']', lambda match: "]},", str)
+str = re.sub(r",'children':\[\]", lambda match: "", str)
 exec('a='+str)
 Json = json.dumps(a[0])
 output = open("../js-tree/"+sys.argv[1]+".json", "w")
